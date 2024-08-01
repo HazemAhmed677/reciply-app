0688bf316f071d551399d95af3de0c4d0281982c
Hazem <hzm@gmail.com>
2024-08-01 03:47:46 +0300

    create splash view & add go router

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const ReciplyApp(),
+  );
+}
+
+class ReciplyApp extends StatelessWidget {
+  const ReciplyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
